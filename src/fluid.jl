303a47997c57f622b84a77f8d05059efb1149175
Junjie Yang <yangjunjie0320@gmail.com>
2024-05-07 00:32:50 +0800

    update

--- src/fluid.jl
+++ src/fluid.jl
@@ -1,7 +1,18 @@
 # [Step 1] define the boltzmann model 
 """
-    AbstractLatticeBoltzmannModel{D, N}
+    AbstractLatticeBoltzmannConfiguration{D, N}
 
+An abstract type representing a lattice Boltzmann configuration 
+in `D` dimensions with `N` velocities.
+"""
+abstract type AbstractLatticeBoltzmannConfiguration{D, N} end
 
 """
-abstract type AbstractLBConfig{D, N} end
+    D2Q9 <: AbstractLatticeBoltzmannConfiguration{2, 9}
+
+D2Q9 lattice Boltzmann model, which is defined in 2-dimensional space
+"""
+
+struct D2Q9 <: AbstractLatticeBoltzmannConfiguration{2, 9} end
+
+# `directions` returns
