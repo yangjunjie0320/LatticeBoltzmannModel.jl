f1c601116110bd7f338bb994cfbdef7c38c9e039
Junjie Yang <yangjunjie0320@gmail.com>
2024-05-07 13:53:49 +0800

    docs

--- README.md
+++ README.md
@@ -4,3 +4,19 @@
 [![Dev](https://img.shields.io/badge/docs-dev-blue.svg)](https://yangjunjie0320.github.io/LatticeBoltzmannModel.jl/dev/)
 [![Build Status](https://github.com/yangjunjie0320/LatticeBoltzmannModel.jl/actions/workflows/CI.yml/badge.svg?branch=main)](https://github.com/yangjunjie0320/LatticeBoltzmannModel.jl/actions/workflows/CI.yml?query=branch%3Amain)
 [![Coverage](https://codecov.io/gh/yangjunjie0320/LatticeBoltzmannModel.jl/branch/main/graph/badge.svg)](https://codecov.io/gh/yangjunjie0320/LatticeBoltzmannModel.jl)
+
+This package implements [Lattice Boltzmann Model (LBM)](https://physics.weber.edu/schroeder/fluids/) 
+for fluid dynamics simulation in `julia`.  The code closely follows the corresponding part in
+[Scientific Computing for Physicists](https://github.com/GiggleLiu/ScientificComputingDemos/tree/main/LatticeBoltzmannModel) by [@GiggleLiu](https://github.com/GiggleLiu).
+
+## Contents
+* D2Q9 model - a 2D LBM with 9 velocities
+* Fluid dynamics visualization
+
+## Get started
+
+Please clone this repository to your local machine, and then run the following commands in the terminal,
+```bash
+$ make init-LatticeBoltzmannModel  # Initialize the environment
+$ make example-LatticeBoltzmannModel  # Run the examples
+```
--- src/fluid.jl
+++ src/fluid.jl
@@ -1,4 +1,4 @@
-# [Step 1] define the boltzmann model 
+# [Step 1] Define the boltzmann model 
 """
     AbstractLatticeBoltzmannConfiguration{D, N}
 
@@ -10,9 +10,48 @@ abstract type AbstractLatticeBoltzmannConfiguration{D, N} end
 """
     D2Q9 <: AbstractLatticeBoltzmannConfiguration{2, 9}
 
-D2Q9 lattice Boltzmann model, which is defined in 2-dimensional space
+D2Q9 lattice Boltzmann model, which is defined in 2-dimensional space,
+and has 9 velocities.
 """
-
 struct D2Q9 <: AbstractLatticeBoltzmannConfiguration{2, 9} end
 
 # `directions` returns the all the directions
+function directions(::D2Q9) :: Tuple{Point2D}
+    return (
+        Point( 1,  1), Point(-1,  1),
+        Point( 1,  0), Point( 0, -1),
+        Point( 0,  0), Point( 0,  1),
+        Point(-1,  0), Point( 1, -1),
+        Point(-1, -1),
+    )
+end
+
+# [Step 2] Define the Cell type for storing the state
+# directions[k] is the opposite of directions[flip_direction_index(k)
+function flip_direction_index(::D2Q9, i::Int)
+    return 10 - i
+end
+
+# the density of the fluid, each component is the density of a velocity
+struct Cell{N, T <: Real}
+    density::NTuple{N, T}
+end
+
+# the total density of the fluid
+density(c::Cell) = sum(cell.density)
+# the density of the fluid in a specific direction,
+# where the direction is an integer
+density(c::Cell, direction::Int) = c.density[direction]
+
+"""
+    get_momentum(lb::AbstractLBConfig, c::Cell)
+
+Compute the momentum of the fluid from the density of the fluid.
+"""
+function get_momentum(config::AbstractLatticeBoltzmannConfiguration, c::Cell)
+    return sum([cd * d for (cd, d) in zip(c.density, config |> directions)])
+end
+
+"""
+    equilibrium_density(config::AbstractLatticeBoltzmannConfiguration, rho::Real, u::Point)
+"""
